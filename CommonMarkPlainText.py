f521615dd5a735a9d051c04a89f15aa4f9b7d44f
Joshua Tauberer <jt@occams.info>
2016-05-10 14:23:51 -0400

    correct and simplify indentation handling: it is always emitted on the block node within the item, not at the start of the item

--- CommonMarkPlainText.py
+++ CommonMarkPlainText.py
@@ -3,32 +3,51 @@
 
 import CommonMark.render.renderer
 
+
 class RawHtmlNotAllowed(ValueError):
     def __init__(self):
         super(RawHtmlNotAllowed, self).__init__("Raw HTML cannot be rendered by the plain text renderer.")
 
+
+class ListBlock:
+    def __init__(self, list_type, start_value):
+        self.list_type = list_type
+        self.value = start_value
+    def __str__(self):
+        # Does not cause indentation.
+        return ""
+
+
+class ItemBullet:
+    def __init__(self, listblock):
+        self.listblock = listblock
+        self.emitted = False
+    def __str__(self):
+        # A bullet is emitted exactly once.
+        if not self.emitted:
+            self.emitted = True
+            return "* "
+
+        # After that, it is just emitted as indentation.
+        else:
+            return "  "
+
+
 class CommonMarkPlainTextRenderer(CommonMark.render.renderer.Renderer):
     def __init__(self):
         self.block_indent = []
-        self.second_block_indent = []
 
-    def hold_block_indent(self):
-        # Skip indentation on the next block.
-        self.second_block_indent = self.block_indent + self.second_block_indent
-        self.block_indent = []
-    def restore_block_indent(self):
-        # A block was emitted. Restore intentation.
-        self.block_indent.extend(self.second_block_indent)
-        self.second_block_indent = []
+    def render_indent(self, next_bullet=False):
+        self.lit("".join(str(b) for b in self.block_indent))
 
     def text(self, node, entering=None):
         self.out(node.literal)
     def softbreak(self, node=None, entering=None):
         self.out("\n")
-        self.out("".join(self.block_indent))
+        self.render_indent()
     def linebreak(self, node=None, entering=None):
         self.out("\n")
-        self.out("".join(self.block_indent))
+        self.render_indent()
     def link(self, node, entering):
         if entering:
             self.link_start = len(self.buf)
@@ -47,25 +66,23 @@ class CommonMarkPlainTextRenderer(CommonMark.render.renderer.Renderer):
         self.out("**") # same symbol entering & existing
     def paragraph(self, node, entering):
         if entering:
-            self.out("".join(self.block_indent))
-            self.restore_block_indent()
+            self.render_indent()
         else:
             self.out("\n\n")
     def heading(self, node, entering):
         if entering:
-           self.out("".join(self.block_indent))
-           self.heading_start = len(self.buf)
+            self.render_indent()
+            self.heading_start = len(self.buf)
         else:
             heading_chars = ["#", "=", "-"]
             if node.level <= len(heading_chars):
                 heading_len = len(self.buf) - self.heading_start
                 self.out("\n")
-                self.out("".join(self.block_indent))
+                self.render_indent()
                 self.out(heading_chars[node.level-1] * heading_len)
             self.out("\n")
-            self.out("".join(self.block_indent))
+            self.render_indent()
             self.out("\n")
-            self.restore_block_indent()
     def code(self, node, entering):
         self.out("```") # better way to render this?
         self.out(node.literal)
@@ -73,42 +90,42 @@ class CommonMarkPlainTextRenderer(CommonMark.render.renderer.Renderer):
     def code_block(self, node, entering):
         max_line_len = max([len(line.replace("\t", "    ")) for line in node.literal.split("\n")])
         # open code block
-        self.out("".join(self.block_indent))
+        self.render_indent()
         self.out("-" * max_line_len + "\n")
-        self.restore_block_indent()
         # each line, with indentation
         lines = node.literal.split("\n")
         while len(lines) > 0 and lines[-1] == "": lines.pop(-1)
         for line in lines:
-            self.out("".join(self.block_indent).replace("*", " ")) # don't emit new bullets
+            self.render_indent()
             self.out(line + "\n")
         # close code block
-        self.out("".join(self.block_indent))
+        self.render_indent()
         self.out("-" * max_line_len + "\n\n")
     def thematic_break(self, node, entering):
+        self.render_indent()
         self.out("-" * 60)
         self.out("\n\n")
-        self.restore_block_indent()
     def block_quote(self, node, entering):
         if entering:
             self.block_indent.append("> ")
         else:
             self.block_indent.pop(-1)
-            self.restore_block_indent()
     def list(self, node, entering):
-        # tagname = 'ul' if node.list_data['type'] == 'bullet' else 'ol'
-        # start = node.list_data['start']
-        # if start is not None and start != 1:
-        pass
+        if entering:
+            self.block_indent.append(ListBlock(node.list_data['type'], node.list_data['start']))
+        else:
+            self.block_indent.pop(-1)
     def item(self, node, entering):
         if entering:
-            # TODO: numbered lists
-            self.out("".join(self.block_indent))
-            self.out("* ")
-            self.block_indent.append("  ")
-            self.hold_block_indent()
+            # Find the ListBlock that was most recently added to self.block_indent.
+            parent_list = [b for b in self.block_indent if isinstance(b, ListBlock)][-1]
+            self.block_indent.append(ItemBullet(parent_list))
+            self.item_start = len(self.buf)
         else:
-            self.restore_block_indent()
+            if len(self.buf) == self.item_start:
+                # Always emit a bullet even if there was no content.
+                self.render_indent()
+                self.out("\n\n")
             self.block_indent.pop(-1)
 
     def html_inline(self, node, entering):
