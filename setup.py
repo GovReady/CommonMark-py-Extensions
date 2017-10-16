f19239211d0f533fc60b7a1e042b6a7d3a0e1508
Joshua Tauberer <jt@occams.info>
2017-10-16 16:03:19 -0400

    bump version, fix metadata

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='commonmark_to_text',
-    version='0.0.1',
+    version='0.0.2',
     author=u'Joshua Tauberer',
     author_email=u'jt@occams.info',
     packages = find_packages(),
     py_modules = ['CommonMarkPlainText'],
-    url='https://github.com/joshdata/commonmark-to-text',
+    url='https://github.com/JoshData/commonmark-py-plaintext',
     license='CC0 (copyright waived)',
     description='A CommonMark renderer that outputs pretty plain text or compliant CommonMark.',
     long_description=open("README.rst").read(),
